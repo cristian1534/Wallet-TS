a593c13d9c9ec87e6fde3ab5ffd9e57b57950cdb
cristian1534 <cmachuca32@gmail.com>
2023-08-29 22:10:25 +0000

    Deployment with TS.

--- src/user/infrastructure/database/mysql.ts
+++ src/user/infrastructure/database/mysql.ts
@@ -6,7 +6,7 @@ import { createTable } from "../helper/table.creator";
 export const dbConnection = () => {
   return new Promise<mysql.Connection>((resolve, reject) => {
     const connection = mysql.createConnection({
-      host: process.env.HOST,
+      host: process.env.MYSQL_HOST,
       database: process.env.MYSQL_DATABASE,
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
